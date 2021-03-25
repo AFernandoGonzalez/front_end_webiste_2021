b2c3361154285c22c15b567661e22ac73828855f
Fernando <gonzalezalvaro096@gmail.com>
2021-03-24 20:05:59 -0400

    mobile version 1/2 done

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-// const loadText = document.getElementById('#loading-text')
-// const bg = document.getElementById('#img')
+window.addEventListener('load', function(){
+    const loader = document.querySelector('.loader')
+    loader.className += ' hidden';
+})
 
 
-// let load = 0
-// let int = setInterval(blurring, 30)
+// navbar
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo');
+const navbar = document.getElementById("navbar");
 
-// function blurring(){
-//     load++
-    
-//     if (load > 99){
-//         clearInterval(int)
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
 
-//     }
+menu.addEventListener('click', mobileMenu);
 
-//     loadText.innerText = `${load}%`
-//     loadText.style.opacity = scale(load, 0, 100, 1, 0)
-//     bg.style.filter = `blur(${scale(load, 0, 100, 30,0)}px)`
-// }
 
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-// }
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
+
 
 
